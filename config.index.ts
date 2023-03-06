cb067439c817fc3546c1e623922b57322653356f
ianklek <ian@brokerinsights.com>
2023-03-06 21:52:15 +0000

    firebase config

--- config.index.ts
+++ config.index.ts
@@ -4,7 +4,7 @@ const targetPath = "./src/environments/environment.ts";
 
 const envConfigFile = `export const environment = {
    production: true,
-   firebase: {
+   firebaseConfig: {
         apiKey: '${process.env.FIREBASE_API_KEY}',
     },
     geocoderKey: '${process.env.GEOCODER_KEY}',
