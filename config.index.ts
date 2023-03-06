74dceb6b10de85d971834d94b09907da50747bef
ianklek <ian@brokerinsights.com>
2023-03-06 21:06:23 +0000

    Config

---
+++ config.index.ts
@@ -0,0 +1,17 @@
+import {writeFile} from "fs";
+
+const targetPath = "./src/environments/environment.prod.ts";
+
+const envConfigFile = `export const environment = {
+   production: true,
+   firebase: {
+        apiKey: '${process.env.FIREBASE_API_KEY}'
+    },
+};
+`;
+
+writeFile(targetPath, envConfigFile, "utf8", (err) => {
+    if (err) {
+        return console.log(err);
+    }
+});
---
+++ src/environments/template.environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  }
+};
+
